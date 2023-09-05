97c3f4afb3f4e217b6241e7cc5745d509589f79d
GEDAHISA <106118710+GEDAHISA@users.noreply.github.com>
2023-09-04 21:51:30 -0300

    inicio do home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Menubar } from '../../components/header/Menubar'
+import { Footer } from '../../components/footer/footer'
+
+export const Home = () => {
+  return (
+    <div>
+        <Menubar/>
+        <Footer/>
+    </div>
+  )
+}
